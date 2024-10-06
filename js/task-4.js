ed3ae5265fd0fcd297e65359fea775a9544c655f
Niko <nikodimstroy@gmail.com>
2024-10-06 15:40:54 +0200

    first push

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+const form = document.querySelector(".login-form");
+const user = [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (
+    event.target.elements.email.value == "" ||
+    event.target.elements.password.value == ""
+  ) {
+    alert("All form fields must be filled in");
+  } else {
+    user.email = event.target.elements.email.value;
+    user.password = event.target.elements.password.value;
+    console.log(user);
+    event.target.reset();
+  }
+});
